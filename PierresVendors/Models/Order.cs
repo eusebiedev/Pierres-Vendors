be02be84536ca55bffc300693abf11bd047f2c98
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-12 10:58:14 -0700

    complete order constructor business logic

--- PierresVendors/Models/Order.cs
+++ PierresVendors/Models/Order.cs
@@ -4,37 +4,36 @@ namespace PierresVendors.Models
 {
   public class Order
   {
-    // private static List<Category> _instances = new List<Category> {};
-    // public string Name { get; set; }
-    // public int Id { get; }
-    // public List<Item> Items { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+    public string Title { get; set; }
+    public string Information { get; set; }
+    public string Date { get; set; }
+    public int Price { get; set; }
+    public int Id { get; }
 
-    // public Category(string categoryName)
-    // {
-    //   Name = categoryName;
-    //   _instances.Add(this);
-    //   Id = _instances.Count;
-    //   Items = new List<Item>{};
-    // }
+    public Order(string title, string information, string date, int price)
+    {
+      Title = title;
+      Information = information;
+      Date = date;
+      Price = price;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
 
-    // public static void ClearAll()
-    // {
-    //   _instances.Clear();
-    // }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
-    // public static List<Category> GetAll()
-    // {
-    //   return _instances;
-    // }
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
 
-    // public static Category Find(int searchId)
-    // {
-    //   return _instances[searchId-1];
-    // }
-
-    // public void AddItem(Item item)
-    // {
-    //   Items.Add(item);
-    // }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
