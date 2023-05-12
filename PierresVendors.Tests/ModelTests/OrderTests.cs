e9eed6a5ee232fdd7c0c422aace530f653cbc459
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-12 14:55:54 -0700

    fix tests to account for int to decimal change

--- PierresVendors.Tests/ModelTests/OrderTests.cs
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -27,12 +27,12 @@ namespace PierresVendors.Tests
       string title = "Pierres Bagels";
       string information = "12 sesame bagels";
       string date = "May 12, 2023";
-      int price = 25;
+      decimal price = 25;
       Order newOrder = new Order(title, information, date, price);
       string result1 = newOrder.Title;
       string result2 = newOrder.Information;
       string result3 = newOrder.Date;
-      int result4 = newOrder.Price;
+      decimal result4 = newOrder.Price;
       Assert.AreEqual(title, result1);
       Assert.AreEqual(information, result2);
       Assert.AreEqual(date, result3);
@@ -45,7 +45,7 @@ namespace PierresVendors.Tests
       string title = "Pierres Bagels";
       string information = "12 sesame bagels";
       string date = "May 12, 2023";
-      int price = 25;
+      decimal price = 25;
       Order newOrder = new Order(title, information, date, price);
       string updatedInformation = "24 poppy seed bagels";
       newOrder.Information = updatedInformation;
@@ -67,11 +67,11 @@ namespace PierresVendors.Tests
       string title1 = "Pierres Bagels";
       string information1 = "12 sesame bagels";
       string date1 = "May 12, 2023";
-      int price1 = 25;
+      decimal price1 = 25;
       string title2 = "Pierres Bagels";
       string information2 = "24 poppy seed bagels";
       string date2 = "May 13, 2023";
-      int price2 = 50;
+      decimal price2 = 50;
       Order newOrder1 = new Order(title1, information1, date1, price1);
       Order newOrder2 = new Order(title2, information2, date2, price2);
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
@@ -85,10 +85,10 @@ namespace PierresVendors.Tests
       string title = "Pierres Bagels";
       string information = "12 sesame bagels";
       string date = "May 12, 2023";
-      int price = 25;
+      decimal price = 25;
       Order newOrder = new Order(title, information, date, price);
       int result = newOrder.Id;
-      Assert.AreEqual(1, result);
+      Assert.AreEqual(0, result);
     }
   }
 }
